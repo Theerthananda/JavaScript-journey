105a2a69cb72f1fb3855a04128b942574b839ac2
Theerthananda <theerthanandagowda@gmail.com>
2026-08-27 11:48:54 +0530

    practice async await

--- Promise Problem/Payment + Order Validation.js
+++ Promise Problem/Payment + Order Validation.js
@@ -12,14 +12,14 @@ function validateCart(cart) {
 }
 
 function createOrder(cart) {
-  return new Promise((resolve,reject) => {
+  return new Promise((resolve, reject) => {
     validateCart(cart)
       .then((cartStatus) => {
         const orderId = cart.length * 100;
         resolve(orderId);
       })
       .catch((err) => {
-        reject(err)
+        reject(err);
       });
   });
 }
@@ -36,14 +36,57 @@ function processPayment(orderId) {
   });
 }
 
-createOrder(cart)
-  .then((orderId) => {
-    console.log("order ID = " + orderId);
-    return processPayment(orderId);
-  })
-  .then((paymentInfo) => {
-    console.log(JSON.stringify(paymentInfo));
-  })
-  .catch((err) => {
-    console.error(err.message);
+// createOrder(cart)
+//   .then((orderId) => {
+//     console.log("order ID = " + orderId);
+//     return processPayment(orderId);
+//   })
+//   .then((paymentInfo) => {
+//     console.log(JSON.stringify(paymentInfo));
+//   })
+//   .catch((err) => {
+//     console.error(err.message);
+//   });
+
+
+function validateUser() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("user valid 1")
+        }, 1000);
+    })
+}
+function getUser() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({
+        name: "Theerthananda",
+        id: 23,
+      });
+    },2000);
   });
+}
+function getUserOrders(userId) {
+    return new Promise((resolve,reject)=> {
+        setTimeout(() => {
+            resolve(["Laptop", "Mobile"]);
+        },4000)
+    })
+}
+
+async function showOrders() {
+    try {
+        let userstatus = await validateUser();
+    console.log(userstatus);
+    let user = await getUser();
+    console.log(user);
+    let orders = await getUserOrders(user.id);
+    console.log(orders)
+    }
+    catch(err) {
+        console.log(err.message);
+    }
+};
+
+
+showOrders();
